f87831359a8ab98b6ae0b33329546d03c5a4d70d
stephtzhang <stephtzhang@gmail.com>
2015-10-21 18:16:08 -0400

    Add js files

---
+++ js/writingHomeFactory.js
@@ -0,0 +1,4 @@
+//   makes the writing home page
+//   ie http://zoeschlanger.com/writing.html
+//   input -- none
+//   output -- appends publications and logos with links to writing pages
---
+++ js/writingPageFactory.js
@@ -0,0 +1,4 @@
+//   makes a writing page for a given publication
+//   ie http://zoeschlanger.com/newsweek.html
+//   input -- name of publication
+//   output -- appends titles and descriptions with links to original articles
